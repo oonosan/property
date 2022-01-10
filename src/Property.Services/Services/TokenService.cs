c322446a422b14723825aee998f149a04a0f6b83
Karen Ono <onokarent@gmail.com>
2022-01-10 20:04:51 -0300

    Create JWT token method

--- src/Property.Services/Services/TokenService.cs
+++ src/Property.Services/Services/TokenService.cs
@@ -1,17 +1,44 @@
 ﻿using Property.ApplicationCore.Entities;
 using Property.ApplicationCore.Interfaces.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
 using System;
 
 namespace Property.Services.Services
 {
     public class TokenService : ITokenService
     {
+        // Symmetric encryption -> only one key is used to both encrypt and decrypt electronic information
+        private readonly SymmetricSecurityKey _key;
+
+        public TokenService(IConfiguration configuration)
+        {
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["TokenKey"]));
+        }
+
         public string CreateToken(User user)
         {
-            throw new NotImplementedException()
+            var claims = new List<Claim>
             {
+                new Claim(JwtRegisteredClaimNames.NameId, user.Username)
+            };
 
-            }
+            var credentials = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(7),
+                SigningCredentials = credentials
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
         }
     }
 }
